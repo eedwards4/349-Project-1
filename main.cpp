ce947f186e83988c9ce0a7ff1eb0c47c84532dc1
eedwards4 <edwardet@sonoma.edu>
2023-02-03 11:41:49 -0800

    Added file readin logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+int main(){
+    int cases; int numships, distance, t1, t2, t3; string line;
+    vector<int> ships;
+    fstream file("test.txt");
+    getline(file, line);
+    cases = stoi(line);
+    //TODO: remember to fix file read
+    for (int i = 0; i <= cases; i++){
+        getline(file, line);
+        // split to numships (first)
+        // split to distance (second)
+        for (int j = 0; j <= numships; j++){
+            getline(file, line);
+            // split t1
+            // split t2
+            // split t3
+            // readin for ship obj
+        }
+    }
+
 }
