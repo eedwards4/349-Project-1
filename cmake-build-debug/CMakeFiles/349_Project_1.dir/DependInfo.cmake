bbcaa70bf85e3128b76c63a1e4925e32d95e434e
Ethan Edwards <evanevansonbusiness@gmail.com>
2023-02-05 12:08:19 -0800

    Unversioned files yaaaay

---
+++ cmake-build-debug/CMakeFiles/349_Project_1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/evane/Documents/CPPprojects/349-Project-1/Ship.cpp" "CMakeFiles/349_Project_1.dir/Ship.cpp.obj" "gcc" "CMakeFiles/349_Project_1.dir/Ship.cpp.obj.d"
+  "C:/Users/evane/Documents/CPPprojects/349-Project-1/main.cpp" "CMakeFiles/349_Project_1.dir/main.cpp.obj" "gcc" "CMakeFiles/349_Project_1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/349_Project_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "349_Project_1.exe"
+  "349_Project_1.exe.manifest"
+  "349_Project_1.pdb"
+  "CMakeFiles/349_Project_1.dir/Ship.cpp.obj"
+  "CMakeFiles/349_Project_1.dir/Ship.cpp.obj.d"
+  "CMakeFiles/349_Project_1.dir/main.cpp.obj"
+  "CMakeFiles/349_Project_1.dir/main.cpp.obj.d"
+  "lib349_Project_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/349_Project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
