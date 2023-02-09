d6eb4bd6d43cff2bda18f951c604ab250a82be98
jwbcoon <jwbcoon@gmail.com>
2023-02-08 22:57:59 -0800

    moved txt files, new file var name, new function

---
+++ README.txt
@@ -0,0 +1,14 @@
+============================
++
++
++   Joe Coon, Ethan Edwards
++   CS 349: Problem Solving in a Team Programming Environment
++       --> Taught by Dr. Gurman Gill; Sonoma State University
++   Ship Depolyer Program
++
++
+============================
+
+To run this program, simply type ./CS349_Project_1 into the command line. Passing arguments 0, 1, or 2
+to the program will activate debug funcitons.
+
--- main.cpp
+++ main.cpp
@@ -6,25 +6,32 @@
 
 using namespace std;
 
+bool outputToFile(int avbShips, fstream& os);
+
 int main(int argc, const char* argv[]) {
     int cases; int numships, distance, t1, t2, t3, avbShips = 0; string line, testLine; bool debugOn = argv[1];
     int correctCount = 0;
     vector<Ship> ships;
-    fstream file("testinput_P1.txt");
-    fstream testOutFile("testoutput_P1.txt");
 
-    getline(file, line);
+    //create environment vars
+    putenv("MYINPUT=textfiles/testinput_P1.txt"); //make sure to replace these with input.txt and output.txt before submitting assignment
+    putenv("MYOUTPUT=textfiles/output.txt");
+    putenv("TESTOUT=textfiles/testoutput_P1.txt");
+
+    fstream inFile(getenv("MYINPUT")), outFile(getenv("MYOUTPUT")), testOutFile(getenv("TESTOUT"));
+
+    getline(inFile, line);
     cases = stoi(line);
 
-    if (file.is_open()) {
+    if (inFile.is_open()) {
         for (int i = 0; i < cases; i++) {
-            getline(file, line);
+            getline(inFile, line);
             // split to numships (first)
             numships = stoi(line.substr(0, line.find(' ')));
             // split to distance (second)
             distance = stoi(line.substr(line.find(' '), line.size() - 1));
             for (int j = 0; j < numships; j++) {
-                getline(file, line);
+                getline(inFile, line);
                 t1 = stoi(line.substr(0, line.find(' '))); // split t1
                 line.erase(0, line.find(' ') + 1);
                 t2 = stoi(line.substr(0, line.find(' '))); // split t2
@@ -38,15 +45,15 @@ int main(int argc, const char* argv[]) {
             }
             
             if (debugOn) {
-                getline(testOutFile, testLine);
+                getline(testOutFile, testLine); // read first line of comparison file
                 if (testOutFile.is_open()) {
-                    if (atoi(argv[1]) == 1) {
+                    if (atoi(argv[1]) == 1) { // test giving percentage success
                         if (stoi(testLine) == avbShips) {
                             correctCount++;
                         }
                     }
                     else if (atoi(argv[1]) == 2) {
-                        if (stoi(testLine) != avbShips) {
+                        if (stoi(testLine) != avbShips) { // test giving first erroneous calculation
                             cout << "First discrepancy found in case "
                             << ++correctCount << ". " << avbShips << " available ships "
                             << "were calculated, but " << testLine << " ships "
@@ -56,19 +63,20 @@ int main(int argc, const char* argv[]) {
                         else {
                             correctCount++;
                         }
-                    }
+                    } // test that visualizes successes and errors
                     else {
                         stoi(testLine) == avbShips ? cout << '.' << endl : cout << "failure" << endl;
                     }
                 }
             }
-            else {
+            else { // behavior when running normally
+                //outputToFile(avbShips, )
                 cout << avbShips << endl;
             }
             avbShips = 0;
         }
 
-        if (debugOn) {
+        if (debugOn) { // output for percent success debug option
             if (atoi(argv[1]) == 1) {
                 cout << (float)(correctCount)/cases << "% success rate with "
                 << correctCount << '/' << cases << " cases matching output.\n";
@@ -76,8 +84,21 @@ int main(int argc, const char* argv[]) {
             testOutFile.close();
         }
 
-        file.close();
+        inFile.close();
+        unsetenv("MYINPUT"); //remove environment variables when exiting program
+        unsetenv("MYOUTPUT");
+        unsetenv("TESTOUT");
+
         return 0;
     }
     
+}
+
+
+bool outputToFile(int avbShips, fstream& os) { //untested framework for output to file
+    if (os.is_open()) {
+        os << avbShips << endl;
+        return true;
+    }
+    return false;
 }
